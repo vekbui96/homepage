27e17f2770355c7967b49a824d2d039b50729ff9
Tommy <tommy.bui1428@gmail.com>
2021-11-18 12:13:21 -0600

    image home update

--- javaProgram.java
+++ javaProgram.java
@@ -2,12 +2,7 @@ import java.applet.*;
 import java.awt.*;
 
 public class javaProgram extends Applet{
-	String message;
-	public void init(){
-		message = "Hello";
-	}
    public void paint(Graphics g){
-   	Graphics2D screen = (Graphics2D) g;
-   	screen.drawString(message, 25, 50);
+	   g.drawString("Welcome", 40, 20);
     }
 }
